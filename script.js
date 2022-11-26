e6c8dc279a56c78cdf158b444d7d856ba6268c25
Vera Udodova <udodowa.vera@yandex.ru>
2022-11-26 23:36:45 +0300

    Mesto_JS_final

--- script.js
+++ script.js
@@ -5,20 +5,48 @@ const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 // кнопка открытия всплывающего окошка
 const openButton = document.querySelector('.profile');
 const popupOpenButtonElement = openButton.querySelector('.profile__edit-button');
+let nameInput = popupElement.querySelector('.popup__name')
+let statusInput = popupElement.querySelector('.popup__status')
+let newName = document.querySelector('.profile__title');
+let newStatus = document.querySelector('.profile__subtitle');
 
 //функция, которая открывает и закрывает окошко
-const togglePopupVisibility = function() {
-popupElement.classList.toggle('popup__is-open')
+const openPopup = function (event) {
+    nameInput.value=newName.textContent;
+    statusInput.value=newStatus.textContent;
+    popupElement.classList.add('popup__is-open');
+    console.log('Open popup clicked');
+    }
+const closePopup = function () {
+    popupElement.classList.remove('popup__is-open');
+    console.log('Close popup clicked');
 }
-//вызываем функцию
-togglePopupVisibility();
 
 //обработчик события
-popupOpenButtonElement.addEventListener('click', function() {
-    popupElement.classList.toggle('popup__is-open')
-});
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+
+//Функция, которая закрашивает сердечко
+let heartElements = document.querySelectorAll('.elements__like-button_disabled')
+
+const heartElementEnabler = function (event) {
+    event.target.classList.add('elements__like-button_active');
+    console.log('Like clicked');
+}
+
+for (let i = 0; i < heartElements.length; i++) {
+    heartElements[i].addEventListener('click', heartElementEnabler);
+}
 
 
-popupCloseButtonElement.addEventListener('click', function() {
-    popupElement.classList.toggle('popup__is-open')
-});
+// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
+function handleFormSubmit(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    newName.textContent = nameInput.value;
+    newStatus.textContent = statusInput.value;
+    closePopup()
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+popupElement.addEventListener('submit', handleFormSubmit);
