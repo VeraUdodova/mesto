419fd6e6c5288c3fb656640b7d93bc7b17b731cb
Vera Udodova <udodowa.vera@yandex.ru>
2022-12-07 21:18:40 +0300

    Mesto_Part2_JS_add_first_cards

--- scripts/index.js
+++ scripts/index.js
@@ -21,6 +21,16 @@ const closePopup = function () {
     console.log('Close popup clicked');
 }
 
+//Ставим "сердечко"
+let heartElements = document.querySelectorAll('.element__like-button')
+const heartElementEnabler = function (event) {
+    event.target.classList.toggle('elements__like-button_active');
+    console.log('Like clicked');
+}
+for (let i = 0; i < heartElements.length; i++) {
+    heartElements[i].addEventListener('click', heartElementEnabler);
+}
+
 // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
 const handleFormSubmit = function (event) {
     event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
@@ -28,6 +38,71 @@ const handleFormSubmit = function (event) {
     newStatus.textContent = statusInput.value;
     closePopup()
 }
+
+//Добавление структуры для первых 6 картинок
+function AddCard(name, link) {
+    const articleElement = document.createElement('article');
+    articleElement.classList.add('element');
+    const trashButton = document.createElement('button');
+    trashButton.classList.add('element__trash-button');
+    trashButton.type='button';
+    const imgElement = document.createElement('img');
+    imgElement.classList.add('element__photo');
+    imgElement.src=link;
+    imgElement.alt=name;
+    const divElement = document.createElement('div');
+    divElement.classList.add('element__title');
+    const h2Element = document.createElement('h2');
+    h2Element.classList.add('element__name');
+    h2Element.textContent=name;
+    const likeButton = document.createElement('button');
+    likeButton.classList.add('element__like-button');
+    likeButton.type='button';
+//Добавляем структуру в DOM
+    const elementsSection = document.querySelector('.elements');
+    articleElement.append(trashButton)
+    articleElement.append(imgElement)
+    articleElement.append(divElement)
+    divElement.append(h2Element)
+    divElement.append(likeButton)
+    elementsSection.prepend(articleElement);
+}
+
+//Первые 6 картинок
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+//Функция добавления первых 6 элементов
+for (let i = 0; i<=5; i=i+1) {
+    AddCard (initialCards[i].name, initialCards[i].link)
+}
+
+
+//Кнопка удаления картинок
+
 
 //обработчик события
 popupOpenButtonElement.addEventListener('click', openPopup);
