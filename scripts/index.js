69d30d5936b6dae2b6640045cd8869859fea65f4
Vera Udodova <udodowa.vera@yandex.ru>
2022-12-11 16:44:59 +0300

    Mesto_Part2_fix_1

--- scripts/index.js
+++ scripts/index.js
@@ -16,8 +16,8 @@ const popupAddPhoto = document.querySelector('.popup-add-photo-block');
 const addPopupElement = document.querySelector('.popup__add-container');
 const openAddImageButtonElement = document.querySelector('.profile__add-photo-button');
 const popupAddImageButtonContainer = document.querySelector('.popup__add-container');
-const addImageTitleInput = document.querySelector('.popup__form-input_add-pic_name_title');
-const addImageUrlInput = document.querySelector('.popup__form-input_add-pic_name_url');
+const addImageTitleInput = document.querySelector('.popup__form-input_name_new-pic-title');
+const addImageUrlInput = document.querySelector('.popup__form-input_name_new-pic-url');
 
 //Константы для полноразмерных картинок
 const popupFullSizeSection = document.querySelector('.popup-fullsize-pic-block')
