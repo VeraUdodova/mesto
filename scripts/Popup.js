fa8ee25ed24eab59fd10adfd8717ba79a68bb562
Vera Udodova <udodowa.vera@yandex.ru>
2023-02-05 13:51:27 +0300

    Mesto_Part5_Popup

---
+++ scripts/Popup.js
@@ -0,0 +1,38 @@
+class Popup{
+    constructor(selector){
+        this._popup = document.querySelector(selector);
+        this._buttonClose = this._popup.querySelector('.popup__close')
+    };
+
+    open(){
+        this._popup.classList.add('popup_opened');
+        this.setEventListeners();
+    };
+
+    close(){
+        this._popup.classList.remove('popup_opened');
+        this._deleteEventListener()
+    };
+
+    _handleEscClose(event) {
+        if (event.key === 'Escape') {
+            this.close()
+        }
+    }
+
+    _handleOverlayClickClose(event) {
+        if (event.target.classList.contains('popup_opened')) {
+            this.close()
+        }
+    }
+
+    _deleteEventListener(){
+        document.removeEventListener("keydown", this._handleEscClose)
+    }
+
+    setEventListeners(){
+        document.addEventListener('keydown', this._handleEscClose);
+        this._popup.addEventListener('mousedown', this._handleOverlayClickClose);
+        this._buttonClose.addEventListener('click', () => this.close());
+    };
+}
