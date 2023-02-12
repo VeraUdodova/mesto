47640a190e5528a1a86f8d22042c1b048bb63be2
Vera Udodova <udodowa.vera@yandex.ru>
2023-02-12 13:30:46 +0300

    Mesto_Project 5_Fix 1

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,28 +1,25 @@
-import {FormValidator} from './FormValidator.js'
-import {formInputElements} from './сonstants.js'
 import {Popup} from './Popup.js'
 
 export class PopupWithForm extends Popup {
-    constructor(selector, handleFormSubmit) {
+    constructor(selector, handleFormSubmit, handleFormClose) {
         super(selector)
         this._handleFormSubmit = handleFormSubmit;
+        this._handleFormClose = handleFormClose;
         this._popupForm = this._popup.querySelector('.popup__form');
-        this._formValidator = new FormValidator(formInputElements, this._popupForm)
-        this._formValidator.enableValidation()
     }
 
     _getInputValues() {
         this._formInputs = this._popup.querySelectorAll('.popup__form-input');
         this._formValues = {}
         this._formInputs.forEach(input => {
-            this._formValues[input.name]= input.value
+            this._formValues[input.name] = input.value
         })
         return this._formValues
     }
 
     setEventListeners() {
         super.setEventListeners();
-        this._popupForm.addEventListener('submit', (event)=>{
+        this._popupForm.addEventListener('submit', (event) => {
             event.preventDefault();
             this._handleFormSubmit(this._getInputValues());
             this._popupForm.reset();
@@ -33,6 +30,6 @@ export class PopupWithForm extends Popup {
     close() {
         super.close();
         this._popupForm.reset();
-        this._formValidator.resetValidation()
+        this._handleFormClose()
     }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import {Section} from '../components/Section.js'
 import {PopupWithForm} from "../components/PopupWithForm.js";
 import {UserInfo} from "../components/UserInfo.js";
 import {PopupWithImage} from "../components/PopupWithImage.js";
-import {initialCards} from "../components/сonstants.js";
+import {initialCards, formInputElements} from "../components/сonstants.js";
+import {FormValidator} from '../components/FormValidator.js'
 import './index.css';
 
 // Объявление переменных
@@ -15,6 +16,8 @@ const profileStatusSelector = '.profile__subtitle';
 const selectorElements = '.elements';
 const formAddSelector = '.popup-add-photo-block';
 const formEditSelector = '.popup-edit-profile-block';
+const formAddElement = document.querySelector('.popup-add-photo-block')
+const formEditElement = document.querySelector('.popup-edit-profile-block')
 
 //Константы для функции добавления картинок
 const selectorTemplate = '#element';
@@ -23,41 +26,49 @@ const openAddImageButton = document.querySelector('.profile__add-photo-button');
 //Константы для полноразмерной картинки
 const popupFullSizeSelector = '.popup-fullsize-pic-block';
 
+const imagePopup = new PopupWithImage(popupFullSizeSelector)
+imagePopup.setEventListeners()
+
+const createCard = function (name, link) {
+    const cardElement = new Card(name, link, selectorTemplate, (name, link) => {
+        imagePopup.open(name, link)
+    })
+    return cardElement.createCard()
+}
+
 const cardsList = new Section({
         items: initialCards, renderer: ({name, link}) => {
-            const cardElement = new Card(name, link, selectorTemplate, (name, link)=>{
-                const imagePopup=new PopupWithImage(popupFullSizeSelector)
-                imagePopup.setEventListeners()
-                imagePopup.open(name, link)
-            })
-            cardsList.addItem(cardElement.createCard())
+            cardsList.addItem(createCard(name, link))
         }
     },
     selectorElements
 );
 
-const userInfo = new UserInfo({nameSelector:profileNameSelector, infoSelector:profileStatusSelector})
+const formEditValidator = new FormValidator(formInputElements, formEditElement)
+const formAddValidator = new FormValidator(formInputElements, formAddElement)
+
+const userInfo = new UserInfo({nameSelector: profileNameSelector, infoSelector: profileStatusSelector})
 
 const formEditPopup = new PopupWithForm(formEditSelector, (formData) => {
-    userInfo.setUserInfo(formData)
-    formEditPopup.close()
-})
+        userInfo.setUserInfo(formData)
+        formEditPopup.close()
+    }, () => {
+        formEditValidator.resetValidation();
+    }
+)
 
 const formAddPopup = new PopupWithForm(formAddSelector, (formData) => {
-    const cardElement = new Card(formData.form_place_name, formData.form_place_url, selectorTemplate, (name, link)=>{
-        const imagePopup=new PopupWithImage(popupFullSizeSelector)
-        imagePopup.setEventListeners()//TODO и без этого работает
-        imagePopup.open(name, link)
-    })
-    cardsList.addItem(cardElement.createCard())
+    cardsList.addItem(createCard(formData.form_place_name, formData.form_place_url))
     formAddPopup.close()
+}, () => {
+    formAddValidator.resetValidation();
 })
 
 
 // обработчик события
 popupProfileEditButton.addEventListener('click', function () {
     formEditPopup.open()
-    const {name,info} = userInfo.getUserInfo()
+    const {name, info} = userInfo.getUserInfo()
     nameProfileInput.value = name;
     statusProfileInput.value = info;
 });
@@ -69,3 +80,5 @@ openAddImageButton.addEventListener('click', function () {
 cardsList.renderItems()
 formEditPopup.setEventListeners()
 formAddPopup.setEventListeners()
+formAddValidator.enableValidation()
+formEditValidator.enableValidation()
